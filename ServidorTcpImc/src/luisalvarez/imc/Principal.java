87262585588c72de9f48d45ed234b7f2e84840c5
luis <luis.alvarezpareja@unicolombo.edu.co>
2025-03-09 15:22:54 -0500

    Primer commit

---
+++ ServidorTcpImc/src/luisalvarez/imc/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package luisalvarez.imc;
+
+import luisalvarez.imc.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author ruisu
+ */
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+
+}
